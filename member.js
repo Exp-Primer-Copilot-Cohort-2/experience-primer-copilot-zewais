170750d9858f0810771bd312cd375c0d9241f18b
Ziad Ewais <arch.ziad.ewais@gmail.com>
2024-09-02 04:52:32 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    return {
+        name: 'John Doe',
+        age: 32,
+        skills: ['JavaScript', 'React', 'Node', 'MongoDB', 'Express', 'GraphQL', 'Apollo Client', 'Apollo Server']
+    }
+}
